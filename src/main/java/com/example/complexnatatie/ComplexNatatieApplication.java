a2368f685fcedf909c8a6c463ff960d755ea56bf
Iosif Radu <raduiosif15@yahoo.com>
2022-05-28 20:44:46 +0300

    Initial commit

---
+++ src/main/java/com/example/complexnatatie/ComplexNatatieApplication.java
@@ -0,0 +1,13 @@
+package com.example.complexnatatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexNatatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexNatatieApplication.class, args);
+    }
+
+}
