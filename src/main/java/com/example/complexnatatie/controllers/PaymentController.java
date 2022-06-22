6c40ff1ae15db913c105d7e8b15b569f0980a8a9
Iosif Radu <raduiosif15@yahoo.com>
2022-06-22 18:15:18 +0300

    [wip] excel and email functions (just sample commented code added)

--- src/main/java/com/example/complexnatatie/controllers/PaymentController.java
+++ src/main/java/com/example/complexnatatie/controllers/PaymentController.java
@@ -1,7 +1,7 @@
 package com.example.complexnatatie.controllers;
 
-import com.example.complexnatatie.controllers.handlers.request.ReportRequest;
 import com.example.complexnatatie.controllers.handlers.request.PaymentRequest;
+import com.example.complexnatatie.controllers.handlers.request.ReportRequest;
 import com.example.complexnatatie.controllers.handlers.responses.PaymentResponse;
 import com.example.complexnatatie.dtos.PaymentDTO;
 import com.example.complexnatatie.services.PaymentService;
@@ -44,5 +44,107 @@ public class PaymentController {
     public ResponseEntity<List<PaymentDTO>> getMonthly(@RequestBody ReportRequest reportRequest) {
         return new ResponseEntity<>(paymentService.getMonthly(reportRequest), HttpStatus.OK);
     }
+
+
+//    public static void emailSender(String toEmail, String subject, String messageText) throws Exception {
+//
+//
+//        Properties props = new Properties();
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.smtp.host", "mail.utcluj.ro");
+//        props.put("mail.smtp.port", "587");
+//        props.put("mail.debug", "true");
+//
+//        Authenticator auth = new Authenticator() {
+//            protected PasswordAuthentication getPasswordAuthentication() {
+//                return new PasswordAuthentication("user", "pass");
+//            }
+//        };
+//
+//        Session session = Session.getInstance(props, auth);
+//
+//        try {
+//
+//            Message message = new MimeMessage(session);
+//            message.setFrom(new InternetAddress("rectorat :))"));
+//            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+//            message.setSubject(subject);
+//
+//
+//            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+//            mimeBodyPart.setContent(messageText, "text/html; charset=utf-8");
+//
+//
+//            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+//            attachmentBodyPart.attachFile(new File("path/to/file"));
+//
+//
+//            Multipart multipart = new MimeMultipart();
+//            multipart.addBodyPart(mimeBodyPart);
+//
+//            multipart.addBodyPart(attachmentBodyPart);
+//
+//            message.setContent(multipart);
+//
+//            Transport.send(message);
+//
+//            System.out.println("Message Sent.");
+//        } catch (MessagingException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//
+//
+//    }
+//
+//
+//
+//
+//    // create file
+//
+//    serverPath = servletRequest.getSession().getServletContext().getRealPath("/");
+//    File reject = new File(serverPath+"/reject.xlsx");
+//
+//		System.out.println(reject.getAbsolutePath());
+//
+//    XSSFWorkbook rejectXLSX = new XSSFWorkbook(new FileInputStream(reject));
+//
+//    final XSSFSheet sheet = rejectXLSX.getSheetAt(0);
+//
+////    int last = sheet.getLastRowNum();
+////
+////		for(int i=1; i<last; i++) {
+////
+////        try {
+////            XSSFRow row = sheet.getRow(i);
+////            sheet.removeRow(row);
+////        }
+////        catch (Exception e) {
+////
+////        }
+////    }
+//
+//    Iterator<Customer> iter = rejectList.iterator();
+//
+//    // row 0 cap tabel
+//
+//
+//    int rowNr = 1;
+//		while(iter.hasNext())
+//
+//    {
+//        customer = iter.next();
+//        row = sheet.createRow(rowNr);
+//
+//        cell = row.createCell(0);
+//        cell.setCellValue(customer.getUniqueCode());
+//
+//        rowNr++;
+//    }
+//
+//    FileOutputStream fos = new FileOutputStream(reject);
+//
+//		rejectXLSX.write(fos);
+//		fos.close();
 
 }
