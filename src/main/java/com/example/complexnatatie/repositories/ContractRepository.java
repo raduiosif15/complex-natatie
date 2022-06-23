b2057c6061f3903de50d0b88953503edf4126691
Iosif Radu <raduiosif15@yahoo.com>
2022-06-23 21:23:44 +0300

    get contract monthly statistic

--- src/main/java/com/example/complexnatatie/repositories/ContractRepository.java
+++ src/main/java/com/example/complexnatatie/repositories/ContractRepository.java
@@ -20,4 +20,12 @@ public interface ContractRepository extends JpaRepository<Contract, Integer> {
             "WHERE contract.customerId = :customerId ")
     List<Contract> getAllByCustomerId(int customerId);
 
+    @Query(nativeQuery = true,
+            value = "SELECT COUNT(*), customer_type, to_char(start_date, 'Mon') as mon " +
+                    "FROM contract " +
+                    "WHERE contract.start_date < CURRENT_DATE " +
+                    "  AND contract.end_date > CURRENT_DATE " +
+                    "AND extract(year from contract.start_date) = :year " +
+                    "group by 2, 3 ")
+    List<Object[]> getActiveMonthStatisticForYear(int year);
 }
