71435f9b11963cf187120ffc1d1abfcd9c26c650
Iosif Radu <raduiosif15@yahoo.com>
2022-06-24 17:27:31 +0300

    heroku servlet path#4

--- src/main/java/com/example/complexnatatie/services/EmbeddedServletContainerConfig.java
+++ src/main/java/com/example/complexnatatie/services/EmbeddedServletContainerConfig.java
@@ -12,9 +12,9 @@ public class EmbeddedServletContainerConfig implements WebServerFactoryCustomize
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
         // local
-        factory.setDocumentRoot(new File("/private/var/folders/k7/bjstfnxj7xg3709mh0pjv7br0000gn/T/tomcat-docbase.8080.6706630708292657697/"));
+//        factory.setDocumentRoot(new File("/private/var/folders/k7/bjstfnxj7xg3709mh0pjv7br0000gn/T/tomcat-docbase.8080.6706630708292657697/"));
 //        server
-//        factory.setDocumentRoot(new File("/tmp/tomcat-docbase.8080.6706630708292657697/"));
+        factory.setDocumentRoot(new File("/tmp/tomcat-docbase.8080.6706630708292657697/"));
 
     }
 
